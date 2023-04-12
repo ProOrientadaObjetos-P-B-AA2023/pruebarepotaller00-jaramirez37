6e7d214ae0c0fcd3515d04d3856b9713a0b2df3d
Joseph Ramirez <73543524+jaramirez37@users.noreply.github.com>
2023-04-12 17:00:49 -0500

    Add files via upload

---
+++ Demo2/src/demo2/Demo2.java
@@ -0,0 +1,14 @@
+package demo2;
+
+/**
+ *
+ * @author SALAI
+ */
+public class Demo2 {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Loja, una ciudad llena de arte y naturaleza con 400322");
+    }
+    
+}
